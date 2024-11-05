eb9e6616eddbd47dd2ef05750594945493a23149
Genkai1604 <kebacbeo@gmail.com>
2024-11-05 20:35:45 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import ssl
+import json
+import time
+import uuid
+from loguru import logger
+import websockets  # Thêm dòng này
+
+async def connect_to_wss(user_id):
+    device_id = str(uuid.uuid4())
+    logger.info(device_id)
+    while True:
+        try:
+            await asyncio.sleep(1)  # Đặt khoảng thời gian mong muốn
+            custom_headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+            }
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            uri = "wss://proxy.wynd.network:4444/"
+            async with websockets.connect(uri, ssl=ssl_context, extra_headers={
+                "Origin": "chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi",
+                "User-Agent": custom_headers["User-Agent"]
+            }) as websocket:
+                async def send_ping():
+                    while True:
+                        send_message = json.dumps(
+                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
+                        logger.debug(send_message)
+                        await websocket.send(send_message)
+                        await asyncio.sleep(30)  # Đặt khoảng thời gian mong muốn
+
+                send_ping_task = asyncio.create_task(send_ping())
+                try:
+                    while True:
+                        response = await websocket.recv()
+                        message = json.loads(response)
+                        logger.info(message)
+                        if message.get("action") == "AUTH":
+                            auth_response = {
+                                "id": message["id"],
+                                "origin_action": "AUTH",
+                                "result": {
+                                    "browser_id": device_id,
+                                    "user_id": user_id,
+                                    "user_agent": custom_headers['User-Agent'],
+                                    "timestamp": int(time.time()),
+                                    "device_type": "extension",
+                                    "version": "4.20.2",
+                                    "extension_id": "lkbnfiajjmbhnfledhphioinpickokdi"
+                                }
+                            }
+                            logger.debug(auth_response)
+                            await websocket.send(json.dumps(auth_response))
+
+                        elif message.get("action") == "PONG":
+                            pong_response = {"id": message["id"], "origin_action": "PONG"}
+                            logger.debug(pong_response)
+                            await websocket.send(json.dumps(pong_response))
+                finally:
+                    send_ping_task.cancel()
+
+        except Exception as e:
+            logger.error(f"Error: {str(e)}")
+            continue
+
+async def main():
+    user_id = '2nQUPOKhUwgkp4qNatcBYqlrguk'  # Thay bằng User ID của bạn
+    await connect_to_wss(user_id)
+
+if __name__ == '__main__':
+    asyncio.run(main())
